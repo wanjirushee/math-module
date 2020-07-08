dc47637373b76cb6c12cac901176282fd1958e8f
sheelyzahwanjiru <sheelyzahwanjiru@gmail.com>
2020-07-08 19:07:07 +0300

    my first import python module

---
+++ index.py
@@ -0,0 +1,10 @@
+  # created by sheliza wanjiru.
+
+from math import sqrt as square_root
+print("               squareIT!            ")
+
+num = float(input("  Enter a number and find square root!\n"))
+
+square=str((square_root(num)))
+
+print("\n Your square root is " + square + " ! ")
